7052fd52ea317ba32ca912609a4ae76e45eacc22
Bardo91 <19bardo91@gmail.com>
2021-07-21 14:43:55 +0200

    Added an example to try to improve the initialization of DAL in case of first connection fails.

---
+++ examples/takeoff_and_land/main.cpp
@@ -0,0 +1,45 @@
+//---------------------------------------------------------------------------------------------------------------------
+//  DJI ABSTRACTION LAYER
+//---------------------------------------------------------------------------------------------------------------------
+//  Copyright 2019 Manuel Pérez Jiménez (a.k.a. manuoso) manuperezj@gmail.com
+//---------------------------------------------------------------------------------------------------------------------
+//  Permission is hereby granted, free of charge, to any person obtaining a copy of this software 
+//  and associated documentation files (the "Software"), to deal in the Software without restriction, 
+//  including without limitation the rights to use, copy, modify, merge, publish, distribute, 
+//  sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is 
+//  furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in all copies or substantial 
+//  portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING 
+//  BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND 
+//  NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES 
+//  OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN 
+//  CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//---------------------------------------------------------------------------------------------------------------------
+
+#include <dal/dal.h>
+#include <dal/hal.h>
+#include <chrono>
+#include <thread>
+#include <iostream>
+
+using std::this_thread::sleep_for;
+using namespace std::chrono;
+using namespace std;
+
+int main(int _argc, char ** _argv){
+
+    dal::HAL::Config config;
+
+    for(unsigned i = 0; i < 5 ; i++){
+        cout << "Trying to connect...." << endl;
+        auto autopilot = dal::DAL::create(config);
+        sleep_for(seconds(1));
+    }
+
+    cout << "Could not connect to autopilot" << endl;
+    return -1;
+
+}
